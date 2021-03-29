fbc5e91ce76ac4c0580e87ca3080eb5deca2db0b
zahrakhedhiri <khedhirizahra22@gmail.com>
2021-03-29 16:14:20 +0200

    First Commit

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new AdminBundle\AdminBundle(),
+            new PublicBundle\PublicBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
